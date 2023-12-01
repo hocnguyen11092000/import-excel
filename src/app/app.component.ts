7e8c099cd95f962a373df214f82ac7008e7d84fb
ADMIN\Admin <hocnguyen1109200@gmail.com>
2023-12-01 23:37:36 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {read, utils, writeFile} from "xlsx";
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,46 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'export-excel';
+  movies: any[] = [];
+
+  handleImport($event: any) {
+    const files = $event.target.files;
+    if (files.length) {
+      const file = files[0];
+      const reader = new FileReader();
+      reader.onprogress = (event) => {
+        console.log(event)
+      }
+
+      reader.onload = (event: any) => {
+        const wb = read(event.target.result);
+        const sheets = wb.SheetNames;
+
+        if (sheets.length) {
+          const rows = utils.sheet_to_json(wb.Sheets[sheets[0]]);
+          this.movies = rows;
+        }
+
+        // console.log(event)
+      }
+
+      reader.readAsArrayBuffer(file);
+    }
+
+  }
+
+  handleExport() {
+    const headings = [[
+      'Movie',
+      'Category',
+      'Director',
+      'Rating'
+    ]];
+    const wb = utils.book_new();
+    const ws: any = utils.json_to_sheet([]);
+    utils.sheet_add_aoa(ws, headings);
+    utils.sheet_add_json(ws, this.movies, { origin: 'A2', skipHeader: true });
+    utils.book_append_sheet(wb, ws, 'Report');
+    writeFile(wb, 'Movie Report.xlsx');
+  }
 }
